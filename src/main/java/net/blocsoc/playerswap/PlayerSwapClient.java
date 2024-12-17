7e62677486d288d7c3950082618b0354184edad8
Rory McDermid <2566302M@student.gla.ac.uk>
2024-12-17 17:30:47 +0000

    Initial commit

---
+++ src/main/java/net/blocsoc/playerswap/PlayerSwapClient.java
@@ -0,0 +1,10 @@
+package net.blocsoc.playerswap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayerSwapClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
