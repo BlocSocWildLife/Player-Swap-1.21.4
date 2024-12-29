e2b42f51ee065e25f626013324c1080460bc18ec
Rory McDermid <2566302M@student.gla.ac.uk>
2024-12-29 15:45:34 +0000

    Should function properly now, just needs built and tested

--- src/main/java/net/blocsoc/playerswap/command/GetAllPlayerPositions.java
+++ src/main/java/net/blocsoc/playerswap/command/GetAllPlayerPositions.java
@@ -7,8 +7,12 @@ import net.minecraft.network.packet.s2c.play.PositionFlag;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -24,12 +28,47 @@ public class GetAllPlayerPositions {
     private static int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
 
 
-        List<ServerPlayerEntity> temp = context.getSource().getServer().getPlayerManager().getPlayerList();
+        List<ServerPlayerEntity> allPlayers = context.getSource().getServer().getPlayerManager().getPlayerList();
 
-        temp.getFirst().interactionManager.getGameMode();
+        List<ServerPlayerEntity> survivalPlayers = new ArrayList<>();
 
-        //need to make a list of all survival mode players, randomise it, and then swap
+        for (int i = 0; i < allPlayers.size(); i++) {
+            if(allPlayers.get(i).interactionManager.getGameMode().toString() == "SURVIVAL"){
+                survivalPlayers.add(allPlayers.get(i));
+            }
+        }
+
+        Collections.shuffle(survivalPlayers);
+
+        while (!survivalPlayers.isEmpty()){
+
+            //maybe set to if divisible by 2?
+            if((survivalPlayers.size() > 3) || (survivalPlayers.size() == 2)){
+                teleportPlayer(survivalPlayers.get(0), survivalPlayers.get(1));
+                teleportPlayer(survivalPlayers.get(1), survivalPlayers.get(0));
+                survivalPlayers.removeFirst();
+                survivalPlayers.removeFirst();
+            }
+            else if(survivalPlayers.size() == 3){
+                teleportPlayer(survivalPlayers.get(0), survivalPlayers.get(1));
+                teleportPlayer(survivalPlayers.get(1), survivalPlayers.get(2));
+                teleportPlayer(survivalPlayers.get(2), survivalPlayers.get(0));
+                survivalPlayers.removeFirst();
+                survivalPlayers.removeFirst();
+                survivalPlayers.removeFirst();
+            }
+
+            else{
+                return -1;
+            }
+
+        }
+
         return 1;
 
+    }
+
+    private static void teleportPlayer(ServerPlayerEntity player1, ServerPlayerEntity player2){
+        player1.teleport(player2.getServerWorld(), player2.getX(), player2.getY() + 10, player2.getZ(), EnumSet.noneOf(PositionFlag.class), player2.getYaw(), player2.getPitch(), false);
     }
 }
